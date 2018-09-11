1e33048bf551177ec71a98c492b8f27205442733
ArletPeba <arletdaria.perez@alumnno.uttehuacan.edu.mx>
2018-09-10 19:06:17 -0500

    componentes agregados

--- RestaurantApp/src/app/app.module.ts
+++ RestaurantApp/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ComponentComponent } from './component/component.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipes-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentComponent,
+    RecipesComponent,
+    ShoppingListComponent,
+    RecipesDetailComponent,
+    RecipesListComponent,
+    RecipesItemComponent
   ],
   imports: [
     BrowserModule
