f2c1688f5fd677e250cbfdece0c7ee5315e64069
ArletPeba <arletdaria.perez@alumnno.uttehuacan.edu.mx>
2018-12-11 17:22:59 -0600

    corregido

--- RestaurantApp/src/app/component/shopping-list/shopping-list.component.ts
+++ RestaurantApp/src/app/component/shopping-list/shopping-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
 import { ingredientsService } from '../../services/ingredients.service';
 import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
@@ -9,16 +10,16 @@ import { Subscription } from 'rxjs';
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients : Ingredient[];
-  private subscription: Subscription;
+  private subscriptionvar;
   constructor( private ingredientService:ingredientsService) { }
 
   ngOnInit() {
    this.ingredients = this.ingredientService.getIngredients();
-   this.subscription = this.ingredientService.ingredientsChanged.subscribe(((ingredients: Ingredient[]) => {this.ingredients = ingredients;}));
+   this.subscriptionvar = this.ingredientService.ingredientsChanged.subscribe(((ingredients: Ingredient[]) => {this.ingredients = ingredients;}));
   }
 
   ngOnDestroy(){
-    this.subscription.unsubscribe;
+    this.subscriptionvar.unsubscribe();
   }
   onEditItem(index:number){
     this.ingredientService.startedEditing.next(index);
